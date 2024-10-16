79ed9a110bdb9ee46472529d105ddf17f8f0a8b5
Haru-Tachibana <paloma030415@gmail.com>
2024-10-16 12:58:17 +0100

    Updates

--- Canny edge detection.py
+++ Canny edge detection.py
@@ -1,35 +1,60 @@
-#Canny edge detection
 import matplotlib.pyplot as plt
 import numpy as np 
-import os
 
 from skimage.io import imread, imshow
 from skimage.color import rgb2hsv, rgb2gray
 from skimage.exposure import histogram, cumulative_distribution
 from skimage.filters import threshold_otsu
-
-from scipy import ndimage as ndi 
-from skimage.util import random_noise
 from skimage import feature
 
-my_path = os.path.abspath('/Users/yangyangxiayule/Documents/GitHub/Computer-Modelling-Y3/Lab3-Figures') 
-leaf2 = imread('/Users/yangyangxiayule/Documents/GitHub/Computer-Modelling-Y3/images-lab3/leaf2.jpg', mode='L')
+import os
 
-edges1 = feature.canny(leaf2)
-edges2 = feature.canny(leaf2, sigma=3)
 
-fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 3))
+my_path = os.path.abspath('/Users/yangyangxiayule/Documents/GitHub/Canny-Edge-Detector---Skimage/Output figures')
 
-ax[0].imshow(leaf2, cmap='gray')
-ax[0].set_title('Leaf2 Gray Image', fontsize=10)
-ax[1].imshow(edges1, cmap='gray')
-ax[1].set_title(r'Canny filter, $\sigma=1$', fontsize=10)
-ax[2].imshow(edges2, cmap='gray')
-ax[2].set_title(r'Canny filter, $\sigma=3$', fontsize=10)
+leaf1 = imread('/Users/yangyangxiayule/Documents/GitHub/Canny-Edge-Detector---Skimage/Input figures/leaf1.jpg', mode='L')
+leaf2 = imread('/Users/yangyangxiayule/Documents/GitHub/Canny-Edge-Detector---Skimage/Input figures/leaf2.jpg', mode='L')
 
-for a in ax:
+edges1_leaf1 = feature.canny(leaf1)
+edges2_leaf1 = feature.canny(leaf1, sigma=3)
+
+edges1_leaf2 = feature.canny(leaf2)
+edges2_leaf2 = feature.canny(leaf2, sigma=3)
+
+#leaf1
+fig1, ax1 = plt.subplots(nrows=1, ncols=3, figsize=(8, 3))
+
+ax1[0].imshow(leaf1, cmap='gray')
+ax1[0].set_title('Leaf1 Gray Image', fontsize=10)
+ax1[1].imshow(edges1_leaf1, cmap='gray')
+ax1[1].set_title(r'Canny filter, $\sigma=1$', fontsize=10)
+ax1[2].imshow(edges2_leaf1, cmap='gray')
+ax1[2].set_title(r'Canny filter, $\sigma=3$', fontsize=10)
+
+#leaf2
+fig2, ax2 = plt.subplots(nrows=1, ncols=3, figsize=(8, 3))
+
+ax2[0].imshow(leaf2, cmap='gray')
+ax2[0].set_title('Leaf2 Gray Image', fontsize=10)
+ax2[1].imshow(edges1_leaf2, cmap='gray')
+ax2[1].set_title(r'Canny filter, $\sigma=1$', fontsize=10)
+ax2[2].imshow(edges2_leaf2, cmap='gray')
+ax2[2].set_title(r'Canny filter, $\sigma=3$', fontsize=10)
+
+
+for a in ax1:
     a.axis('off')
 
-fig.tight_layout()
-plt.show()
-fig.savefig(my_path + '/leaf2-Canny_Edge_detection.png')
+for a in ax2:
+    a.axis('off')
+
+
+fig1.tight_layout()
+fig2.tight_layout()
+
+#optional show plots
+fig1.show()
+fig2.show()
+
+fig1.savefig(my_path + '/leaf1-Canny_Edge_detection.png')
+fig2.savefig(my_path + '/leaf2-Canny_Edge_detection.png')
